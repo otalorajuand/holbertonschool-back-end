d2a8e3708e026a8a30eeb4687016a980e4938970
otalorajuand <otalorajuand@gmail.com>
2022-12-12 18:50:33 +0000

    Addition of task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,24 +6,33 @@ import requests
 import sys
 
 
-url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
-url_name = 'https://jsonplaceholder.typicode.com/users?id='
-response_todo = requests.get(url_todo + sys.argv[1])
-response_name = requests.get(url_name + sys.argv[1])
+def gather_data_from_api():
+    """
+        This function gather data from an api
+    """
 
-content_todo = list(response_todo.json())
-content_name = list(response_name.json())
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
+    url_name = 'https://jsonplaceholder.typicode.com/users?id='
+    response_todo = requests.get(url_todo + sys.argv[1])
+    response_name = requests.get(url_name + sys.argv[1])
 
-task_completed = 0
-total_tasks = 0
-completed_tasks = []
-for elem in content_todo:
-    if elem['completed']:
-        task_completed += 1
-        completed_tasks.append(elem['title'])
-    total_tasks += 1
+    content_todo = list(response_todo.json())
+    content_name = list(response_name.json())
 
-print("Employee {} is done with tasks({}/{}):".format(
-    content_name[0]['name'], task_completed, total_tasks))
-for elem in completed_tasks:
-    print('\t ' + elem)
+    task_completed = 0
+    total_tasks = 0
+    completed_tasks = []
+    for elem in content_todo:
+        if elem['completed']:
+            task_completed += 1
+            completed_tasks.append(elem['title'])
+        total_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        content_name[0]['name'], task_completed, total_tasks))
+    for elem in completed_tasks:
+        print('\t ' + elem)
+
+
+if __name__ == "__main__":
+    gather_data_from_api()
