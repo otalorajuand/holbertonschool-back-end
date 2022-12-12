397071ee9e4788886d1d38d0e5a076f82144374d
otalorajuand <otalorajuand@gmail.com>
2022-12-12 22:14:49 +0000

    Addition of task 4

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module containts an api request
+"""
+import json
+import requests
+
+
+def gather_data_from_api():
+    """
+        This function gather data from an api
+    """
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    response_todo = requests.get(url_todo)
+    response_users = requests.get(url_users)
+
+    content_todo = list(response_todo.json())
+    content_users = list(response_users.json())
+
+    result = {}
+    for user in content_users:
+
+        attributes = []
+        for elem in content_todo:
+            aux_dict = {}
+            aux_dict['username'] = content_users[0]['username']
+            aux_dict['task'] = elem['title']
+            aux_dict['completed'] = elem['completed']
+
+            if user['id'] == elem['userId']:
+                attributes.append(aux_dict)
+
+        result[user['id']] = attributes
+
+    jsonString = json.dumps(result)
+    with open('todo_all_employees.json', 'w') as f:
+        f.write(jsonString)
+
+
+if __name__ == "__main__":
+    gather_data_from_api()
