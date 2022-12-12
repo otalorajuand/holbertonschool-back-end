12ffbd339ce051662648d23f8a3a6fdc7df108fa
otalorajuand <otalorajuand@gmail.com>
2022-12-12 18:46:16 +0000

    Addition of task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests, sys
+
+
+url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
+url_name = 'https://jsonplaceholder.typicode.com/users?id='
+response_todo = requests.get(url_todo+sys.argv[1])
+response_name = requests.get(url_name+sys.argv[1])
+
+content_todo = list(response_todo.json())
+content_name = list(response_name.json())
+
+task_completed = 0
+total_tasks = 0
+completed_tasks = []
+for elem in content_todo:
+    if elem['completed']:
+        task_completed += 1
+        completed_tasks.append(elem['title'])
+    total_tasks += 1
+
+print("Employee {} is done with tasks({}/{}):".format(content_name[0]['name'], task_completed, total_tasks))
+for elem in completed_tasks:
+    print('\t '+elem)
